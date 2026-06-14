bd1c8c58047c484cd5eebb29f029906eac8dcaae
sumit <sumit.ingale@algoocean.com>
2026-06-14 19:21:14 +0530

    auth provider exported

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as FormLayout } from "../components/form-layout";
 export { default as EmailPasswordForm } from "../components/email-password-form";
+export { AuthProvider } from '../context/auth.provider'
