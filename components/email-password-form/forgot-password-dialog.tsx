ae88d6e8067d50ac59b19584bc1a2ae31629a0b9
sumit <sumit.ingale@algoocean.com>
2026-06-14 17:38:18 +0530

    forgot password api integration

--- components/email-password-form/forgot-password-dialog.tsx
+++ components/email-password-form/forgot-password-dialog.tsx
@@ -11,18 +11,18 @@ import useForgotPassword from "@/hooks/use-forgot-password";
 import { Input } from "../ui/input";
 
 const ForgotPasswordModal = () => {
-
     const { openForgotPasswordModal, setOpenForgotPasswordModal } = useUtilsStore();
     const { form, onSubmit } = useForgotPassword();
 
     const handleClose = () => {
-        setOpenForgotPasswordModal(false)
-        form.reset()
-    }
+        setOpenForgotPasswordModal(false);
+        form.reset();
+    };
+
     return (
         <Dialog open={openForgotPasswordModal} onOpenChange={setOpenForgotPasswordModal}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-                <DialogContent className="sm:max-w-sm">
+            <DialogContent className="sm:max-w-sm">
+                <form className="space-y-3" onSubmit={form.handleSubmit(onSubmit)}>
                     <DialogHeader>
                         <DialogTitle>Forgot Password</DialogTitle>
                     </DialogHeader>
@@ -32,14 +32,14 @@ const ForgotPasswordModal = () => {
                         isError={!!form.formState.errors.email}
                         errorMessage={form.formState.errors.email?.message}
                     />
-                    <DialogFooter className="flex items-center gap-2" >
-                        <Button type="submit" className="flex-1" variant={'destructive'} >Reset</Button>
-                        <Button className="flex-1" onClick={handleClose} variant="outline" >Cancel</Button>
-                    </DialogFooter>
-                </DialogContent>
-            </form>
+                    <div className="flex items-center gap-2 mt-4">
+                        <Button type="submit" className="flex-1" variant="destructive">Reset</Button>
+                        <Button type="button" className="flex-1" onClick={handleClose} variant="outline">Cancel</Button>
+                    </div>
+                </form>
+            </DialogContent>
         </Dialog>
-    )
-}
+    );
+};
 
 export default ForgotPasswordModal;
--- hooks/use-forgot-password.ts
+++ hooks/use-forgot-password.ts
@@ -1,5 +1,6 @@
 import { forgotPasswordAdmin } from "@/actions/auth.actions"
 import { useAuth } from "@/context/auth.provider"
+import useUtilsStore from "@/store/utils.store"
 import { apiHandler } from "@/utils/api-handler"
 import { forgotPasswordValidationSchema } from "@/validation-schema"
 import { ForgotPasswordFormData } from "@/validation-schema/form-data-types"
@@ -10,7 +11,7 @@ import { toast } from "sonner"
 
 const useForgotPassword = () => {
     const { config } = useAuth()
-
+    const { setOpenForgotPasswordModal } = useUtilsStore()
 
     const form = useForm<ForgotPasswordFormData>({
         mode: "onChange",
@@ -24,8 +25,18 @@ const useForgotPassword = () => {
 
     const onSubmit = async () => {
         await apiHandler(() => forgotPasswordAdmin(form.getValues(), config.forgotPasswordUrl, config.apiUrl), {
-            onSuccess: () => {
+            onSuccess: (res: any) => {
+                if (res.responseCode == 400) {
+                    toast.error(res.message)
+                    return
+                }
                 toast.success("Password reset link sent successfully.")
+                setOpenForgotPasswordModal(false)
+                form.reset()
+
+            },
+            onError: (err) => {
+                console.log(err)
             }
         })
     }
